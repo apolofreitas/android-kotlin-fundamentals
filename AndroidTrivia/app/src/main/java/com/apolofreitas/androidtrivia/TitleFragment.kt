7c858ae8cb119250c7138004be61023ff476c408
Apolo Freitas <apolobrener11@gmail.com>
2021-01-19 00:05:44 -0400

    feat: add navigation

--- AndroidTrivia/app/src/main/java/com/apolofreitas/androidtrivia/TitleFragment.kt
+++ AndroidTrivia/app/src/main/java/com/apolofreitas/androidtrivia/TitleFragment.kt
@@ -1,24 +1,32 @@
 package com.apolofreitas.androidtrivia
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.apolofreitas.androidtrivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
-            inflater,
-            R.layout.fragment_title, container, false
+                inflater,
+                R.layout.fragment_title,
+                container,
+                false
         )
+
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
         return binding.root
     }
 }
